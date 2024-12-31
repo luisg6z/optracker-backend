77cdaf46bfe911793847f64de0a8227efeee1130
LuisGonzalez <luisgonzalezsalazar03@gmail.com>
2024-12-31 12:27:45 -0400

    fix: added class tranformer and validation pipe on app

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors()
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    })
+  )
 
   const config = new DocumentBuilder()
     .setTitle('OpTracker API')
