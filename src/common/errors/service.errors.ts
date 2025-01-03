236a8131e510543d3b407fef7ef656ca864611a6
LuisGonzalez <luisgonzalezsalazar03@gmail.com>
2025-01-03 12:31:53 -0400

    fix: return type of jwt and 403 error

--- src/common/errors/service.errors.ts
+++ src/common/errors/service.errors.ts
@@ -7,3 +7,5 @@ export class AlreadyExistsError extends ServiceError {}
 export class UnexpectedError extends ServiceError {}
 
 export class ForeignKeyError extends ServiceError {}
+
+export class UnauthorizedError extends ServiceError {}
