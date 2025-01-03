48b255eb7746984a78fdc04f4a21c44fc9ea5b12
LuisGonzalez <luisgonzalezsalazar03@gmail.com>
2025-01-03 16:09:27 -0400

    fix: change on login response

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { JwtService } from '@nestjs/jwt';
 import { AdminService } from 'src/admin/admin.service';
 import * as bcrypt from 'bcrypt'
 import { UnauthorizedError } from 'src/common/errors/service.errors';
+import { Role } from 'src/common/constants';
 
 @Injectable()
 export class AuthService {
@@ -18,22 +19,24 @@ export class AuthService {
         const nurse = await this.nurseService.findOneByEmail(loginDto.email)
 
         if(nurse && bcrypt.compare(loginDto.password, nurse.password)){
-            const payload = {email: nurse.email, id: nurse.id, role: "Nurse"}
+            const payload = {email: nurse.email, id: nurse.id, role: Role.NURSE}
             const token = await this.jwtService.signAsync(payload)
             return {
                 email: nurse.email,
-                token: token
+                token: token,
+                role: Role.NURSE
             }
         }
 
         const admin = await this.adminService.findOneByEmail(loginDto.email)
 
         if(admin && bcrypt.compare(loginDto.password, admin.password)){
-            const payload = {email: admin.email, id: admin.id, role: "Admin"}
+            const payload = {email: admin.email, id: admin.id, role: Role.ADMIN}
             const token = await this.jwtService.signAsync(payload)
             return {
                 email: admin.email,
-                token: token
+                token: token,
+                role: Role.ADMIN
             }
         }
 
