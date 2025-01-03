564600ae0d369d5a38149a109fd722ecd92da390
LuisGonzalez <luisgonzalezsalazar03@gmail.com>
2025-01-03 16:07:30 -0400

    fix: hotfix storing password

--- src/nurse/nurse.service.ts
+++ src/nurse/nurse.service.ts
@@ -16,14 +16,14 @@ export class NurseService {
 
   async create(createNurseDto: CreateNurseDto) {
     try {
-      const hashedPassword = bcrypt.hash(
+      const hashedPassword = await bcrypt.hash(
         createNurseDto.password,
         Number(process.env.SALT_ROUNDS),
       );
       return await this.prisma.nurse.create({
         data: {
-          password: hashedPassword,
           ...createNurseDto,
+          password: hashedPassword,
         },
       });
     } catch (error) {
@@ -34,7 +34,7 @@ export class NurseService {
           );
         }
       }
-      throw new UnexpectedError('An unexpected error ocurred');
+      throw new UnexpectedError(error.message);
     }
   }
 
