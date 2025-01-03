97c38636b50b6d589c1bd0b1531c7866c11949a6
LuisGonzalez <luisgonzalezsalazar03@gmail.com>
2025-01-03 12:32:48 -0400

    feat: auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, InternalServerErrorException, Post, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { UnauthorizedError } from 'src/common/errors/service.errors';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  async login(){}
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(@Body() loginDto: LoginDto){
+    try {
+      const user = await this.authService.login(loginDto);
+  
+      if(!user) {
+        throw new UnauthorizedError("Invalid credentials")
+      }
+  
+      return user;
+    } catch (error) {
+      if (error instanceof UnauthorizedError) {
+        throw new UnauthorizedException(error.message)
+      }
+      throw new InternalServerErrorException(error.message, {cause: error})
+    }
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { NurseService } from 'src/nurse/nurse.service';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { AdminService } from 'src/admin/admin.service';
+import * as bcrypt from 'bcrypt'
+import { UnauthorizedError } from 'src/common/errors/service.errors';
 
 @Injectable()
 export class AuthService {
-    constructor(){}
+    constructor(
+        private readonly nurseService: NurseService,
+        private readonly adminService: AdminService,
+        private readonly jwtService: JwtService
+    ){}
+
+    async login(loginDto: LoginDto){
+        const nurse = await this.nurseService.findOneByEmail(loginDto.email)
+
+        if(nurse && bcrypt.compare(loginDto.password, nurse.password)){
+            const payload = {email: nurse.email, id: nurse.id, role: "Nurse"}
+            const token = await this.jwtService.signAsync(payload)
+            return {
+                email: nurse.email,
+                token: token
+            }
+        }
+
+        const admin = await this.adminService.findOneByEmail(loginDto.email)
+
+        if(admin && bcrypt.compare(loginDto.password, admin.password)){
+            const payload = {email: admin.email, id: admin.id, role: "Admin"}
+            const token = await this.jwtService.signAsync(payload)
+            return {
+                email: admin.email,
+                token: token
+            }
+        }
+
+        return null
+
+    }
 }
